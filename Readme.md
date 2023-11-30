e7f2c309873f5cfc0be6339681392a840ea5a7eb
yuzawa <busieasy98@gmail.com>
2023-11-30 12:52:55 +0900

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+dockerfile,ymlファイル参照元
+https://qiita.com/art_porokyu/items/8363334c358c67adb61a
+
---
+++ node/app/index.js
@@ -0,0 +1,18 @@
+// パッケージを読み込む
+let http = require('http');
+
+// サーバ機能の初期化
+let server = http.createServer();
+
+// リクエストがきたときに呼び出される86
+
+server.on('request', function(req, res)
+{
+    // 本文（Body部）に文字を表示する
+    res.write('Hello, world!');
+    res.end();
+});
+
+// 指定したIPアドレス、ポート番号でサーバを立てる
+server.listen(8080, '127.0.0.1');
+console.log("aaaa");
