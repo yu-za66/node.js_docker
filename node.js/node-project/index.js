00f30334aa1cfcf405186ab896cdc925c21772e2
yuzawa <busieasy98@gmail.com>
2024-02-27 20:35:55 +0900

    バックエンド完成

--- node.js/node-project/index.js
+++ node.js/node-project/index.js
@@ -1,25 +1,24 @@
-// var express = require('express');
-// var router = express.Router();
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-// module.exports = router;
+require('dotenv').config();
 
 const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
-const port = 4000;
+const port = 3000;
+
+const { networkInterfaces } = require('os');
 
 const connection = mysql.createConnection({
-  host: 'mysql', // MySQLコンテナのサービス名を指定
-  user: 'root',
-  password: 'quaddisaster',
-  database: 'ouyou',
+  host: 'mysql',  
+  user: 'user',
+  password: process.env.DB_PASS, 
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
+
 
 connection.connect((err) => {
   if (err) {
@@ -29,17 +28,42 @@ connection.connect((err) => {
   }
 });
 
-app.get('/', (req, res) => {
-  connection.query('SELECT * FROM 学生表', (err, results) => {
-    if (err) {
-      console.error('Error querying MySQL:', err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.json(results);
-    }
-  });
+let reloadCount = 0;
+
+app.get('/', async (req, res) => {
+  console.log('Handling GET request to /');
+
+
+  // 非同期操作を実行
+  try {
+    // reloadCountを増加
+    reloadCount++;
+
+    const queryResults = await performAsyncOperation('SELECT * FROM user');
+
+    // すべての非同期操作が成功したらレスポンスを送信
+    res.json({ reloadCount, queryResults});
+  } catch (error) {
+    // エラーが発生した場合はエラーレスポンスを送信
+    console.error('Error in async operations:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
-app.listen(port, 'localhost', () => {
+// SQLクエリを実行する非同期関数
+function performAsyncOperation(query) {
+  return new Promise((resolve, reject) => {
+    connection.query(query, (err, results) => {
+      if (err) {
+        console.error('Error querying MySQL:', err);
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+}
+
+app.listen(port, '0.0.0.0', () => {
   console.log(`App listening at http://localhost:${port}`);
 });
